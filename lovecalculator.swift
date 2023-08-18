0611b140a320a79bc2f1a5ec1f4918cf1df86333
Rakesh Ghatvisave <rakesh.rgt86@gmail.com>
2023-08-18 16:00:41 +0800

    Update lovecalculator.swift

--- lovecalculator.swift
+++ lovecalculator.swift
@@ -1,10 +1,18 @@
 
+
 func loveCalculator(){
     let loveScore = Int.random(in: 0...100)
     
-    if loveScore == 100 {
+    
+    if loveScore > 80 {
         print("You love each other like Kanye loves Kanye")
     }
+    
+    else if loveScore > 40 {
+        print("you love together like coke and mentos")
+        
+    }
+    
         else {
             print("You will be forever alone")
         }
